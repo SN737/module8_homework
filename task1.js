221673263f50e597f31a128e1db861aac1593a3a
Sergey <92508690+SN737@users.noreply.github.com>
2022-08-16 17:17:44 +0300

    продолжаю решать

--- task1.js
+++ task1.js
@@ -17,24 +17,45 @@ let xmlData = `<list>
 </student>
 </list>`;
 
-let dParser = new DOMParser();
+
+
+let result = {};
+const dParser = new DOMParser();
 let xParser = dParser.parseFromString(xmlData, "text/xml");
-let keyNode = xParser.querySelector("list");
-let studentNode = xParser.querySelector("student");
-let nameNode = xParser.querySelector("name");
-let firstName = xParser.querySelector("first");
-let secondName = xParser.querySelector("second");
-let age = xParser.querySelector("age");
-let prof = xParser.querySelector("prof");
-                                      
 
-let lang = nameNode.getAttribute('lang');
 
-let result = {
-    list: [
-        {name: (firstName.textContent +' '+ secondName.textContent),
-        age: age.textContent, prof:  prof.textContent, lang: lang}
-    ]
+function objCreator(xParser) {
+  
+    let studentNode = xParser.querySelector("student");
+    let nameNode = xParser.querySelector("name");
+    let firstName = xParser.querySelector("first");
+    // console.log(firstName)
+    let secondName = xParser.querySelector("second");
+    let age = xParser.querySelector("age");
+    let prof = xParser.querySelector("prof");
+    let lang = nameNode.getAttribute('lang');
+    let keyNode = xParser.querySelector("list");
+    let keyNodeLen = xParser.querySelectorAll("list");
+   //console.log ( keyNodeLen.length)
 
+    result =  {list:[ (result),         
+            {name: (firstName.textContent +' '+ secondName.textContent),
+            age: age.textContent, prof:  prof.textContent, lang: lang},]
+            
+    };
+    studentNode.remove();
+   // console.log(result)
+   
+
+  
+   if (keyNodeLen.length > 0) {          
+    objCreator(keyNode)
+    }else {return result};
 }
+
+objCreator(xParser)
 console.log(result);
+
+
+
+// console.log (keyNode)
--- task2.js
+++ task2.js
@@ -1,61 +1,17 @@
-let xmlData = `<list>
-<student>
-  <name lang="en">
-    <first>Ivan</first>
-    <second>Ivanov</second>
-  </name>
-  <age>35</age>
-  <prof>teacher</prof>
-</student>
-<student>
-  <name lang="ru">
-    <first>Петр</first>
-    <second>Петров</second>
-  </name>
-  <age>58</age>
-  <prof>driver</prof>
-</student>
-</list>`;
+let jsonData = `{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`;
 
-
-
-let result = {};
-const dParser = new DOMParser();
-let xParser = dParser.parseFromString(xmlData, "text/xml");
-
-
-function objCreator(xParser) {
-  
-    let studentNode = xParser.querySelector("student");
-    let nameNode = xParser.querySelector("name");
-    let firstName = xParser.querySelector("first");
-    // console.log(firstName)
-    let secondName = xParser.querySelector("second");
-    let age = xParser.querySelector("age");
-    let prof = xParser.querySelector("prof");
-    let lang = nameNode.getAttribute('lang');
-    let keyNode = xParser.querySelector("list");
-    let keyNodeLen = xParser.querySelectorAll("list");
-   //console.log ( keyNodeLen.length)
-
-    result =  {list:[ (result),         
-            {name: (firstName.textContent +' '+ secondName.textContent),
-            age: age.textContent, prof:  prof.textContent, lang: lang},]
-            
-    };
-    studentNode.remove();
-   // console.log(result)
-   
-
-  
-   if (keyNodeLen.length > 0) {          
-    objCreator(keyNode)
-    }else {return result};
-}
-
-objCreator(xParser)
-console.log(result);
-
-
-
-// console.log (keyNode)
+   let employees = JSON.parse(jsonData);
+   console.log(employees);
