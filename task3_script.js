b822a905c1dbadb2a14cfdcd644a50301c087aa9
Sergey <92508690+SN737@users.noreply.github.com>
2022-08-18 17:24:10 +0300

    update

--- task3_script.js
+++ task3_script.js
@@ -1,18 +1,26 @@
 const input = document.querySelector('input');
 const inputBtn = document.querySelector('input[type="button"]');
-const message = document.querySelector('message');
+let  message = document.querySelector('.message');
 
 
 
 inputBtn.addEventListener('click', ()=> {
     if (!input.value.length){
-        alert('exit');
+        alert('введите числовое значение');
         return;
     } else if (input.value>10 || input.value<1) {
-        message.innerHTML = "число вне диапазона от 1 до 10";
-        alert('число вне диапазона от 1 до 10');
+        // alert('число вне диапазона от 1 до 10');
+        message.innerHTML = "<b>число вне диапазона от 1 до 10</b>";
+        
     } else {
-        alert('akajk');
-        xhrReq();
+        alert('вызываем функцию');
+        xhrReq(input.value);
+        
     }
 });
+
+function xhrReq(value){
+    let xhr = new XMLHttpRequest();
+    let url = `https://picsum.photos/v2/list?limit=${value}`;
+    xhr.open('GET', url);
+}
