ad43cb5d4cc5903beb6c02075af461ebdf19c33e
Sergey <92508690+SN737@users.noreply.github.com>
2022-08-18 22:24:58 +0300

    доделал

--- task3_script.js
+++ task3_script.js
@@ -1,22 +1,33 @@
-const input = document.querySelector('input');
+const input = document.querySelector('.input');
 const inputBtn = document.querySelector('input[type="button"]');
 let  message = document.querySelector('.message');
 let picture = document.querySelector('.picture');
-let respArr = [];
 
+input.addEventListener('input', () => {
+    if(!input.value.length){
+        inputBtn.disabled = true;
+    } else{
+        inputBtn.disabled = false;}  
+});
 
 
 inputBtn.addEventListener('click', ()=> {
-    if (!input.value.length){
-        alert('введите числовое значение');
+    if (!input.value.length){        
+        // alert('введите числовое значение');
+        // input.value = '';
         return;
     } else if (input.value>10 || input.value<1) {
-        // alert('число вне диапазона от 1 до 10');
-        message.innerHTML = "<b>число вне диапазона от 1 до 10</b>";
+        
+        message.innerHTML = "<b>Число вне диапазона от 1 до 10</b>";
+        input.value = '';
         
     } else {
         // alert('вызываем функцию');
         xhrReq(input.value);
+        input.value = '';
+        inputBtn.disabled = true;
+
+        
         
     }
 });
@@ -27,20 +38,20 @@ function xhrReq(value){
     xhr.open('GET', url);
     xhr.send();
     xhr.onload = ()=> {
-    respArr = (xhr.response);
-    
-    // return respArr;
-    console.log(respArr);
-    
-    renderImg(respArr);
-    
-    };  
+    let respArr = JSON.parse(xhr.response);    
+    renderImg(respArr);    
+    };     
+    xhr.onerror = ()=> {
+        alert('Ошибка запроса');
+      };
+      
 }
 
 function renderImg(respArr) {
+
+    picture.innerHTML= '';
         respArr.forEach(item => {
-        picture.innerHTML = `<div><img src="${item.download_url}"/></div>`;
-        console.log(1);
-        
+        let itenImg= `<div class = "img"><img src="${item.download_url}"/></div>`;
+        picture.innerHTML += itenImg;  
     });
 }
