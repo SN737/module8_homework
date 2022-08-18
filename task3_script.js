ddaccbf32811e74889f8cdb80714289cd02fd2a0
Sergey <92508690+SN737@users.noreply.github.com>
2022-08-18 20:58:37 +0300

    update

--- task3_script.js
+++ task3_script.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('input');
 const inputBtn = document.querySelector('input[type="button"]');
 let  message = document.querySelector('.message');
+let picture = document.querySelector('.picture');
+let respArr = [];
 
 
 
@@ -13,7 +15,7 @@ inputBtn.addEventListener('click', ()=> {
         message.innerHTML = "<b>число вне диапазона от 1 до 10</b>";
         
     } else {
-        alert('вызываем функцию');
+        // alert('вызываем функцию');
         xhrReq(input.value);
         
     }
@@ -23,4 +25,22 @@ function xhrReq(value){
     let xhr = new XMLHttpRequest();
     let url = `https://picsum.photos/v2/list?limit=${value}`;
     xhr.open('GET', url);
+    xhr.send();
+    xhr.onload = ()=> {
+    respArr = (xhr.response);
+    
+    // return respArr;
+    console.log(respArr);
+    
+    renderImg(respArr);
+    
+    };  
+}
+
+function renderImg(respArr) {
+        respArr.forEach(item => {
+        picture.innerHTML = `<div><img src="${item.download_url}"/></div>`;
+        console.log(1);
+        
+    });
 }
